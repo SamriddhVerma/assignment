ff09ad562ccace693fd9fc379fad5e8ef71a7d4e
SamriddhVermaSRM <samriddh_be22it132@srmcem.ac.in>
2025-12-30 02:30:22 +0530

    updated datatypes to be of appropriate size

--- mysql.sql
+++ mysql.sql
@@ -1,11 +1,11 @@
 CREATE TABLE schools.schools (
 	id INT auto_increment NOT NULL,
 	name VARCHAR(511) NULL,
-	address TEXT NULL,
-	state TEXT NULL,
-	city TEXT NULL,
+	address VARCHAR(511) NULL,
+	state VARCHAR(255) NULL,
+	city VARCHAR(100) NULL,
 	contact BIGINT NULL,
-	image TEXT NULL,
-	email_id TEXT NULL,
+	image VARCHAR(511) NULL,
+	email_id VARCHAR(255) NULL,
 	CONSTRAINT schools_pk PRIMARY KEY (id)
 )
